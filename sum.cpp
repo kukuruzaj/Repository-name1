adf2039b02dcda0824999eef4ef12a730166721d
kukuruzaj <kukuruzaj@gmail.com>
2025-01-10 19:35:43 +0300

    Add files via upload

---
+++ sum.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int firstNumber, secondNumber, sum;
+
+	cout<<"Enter two integers: ";
+	cin >> firstNumber >> secondNumber;
+	sum = firstNumber + secondNumber;
+	cout << firstNumber << " + " << secondNumber << " = " << sum;
+	return 0;
+}
+
